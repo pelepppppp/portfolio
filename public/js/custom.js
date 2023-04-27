b94e294892b6233dbd9026581a384078829b51f6
Philip Bautista <philipbautista928@gmail.com>
2023-04-27 14:48:34 +0800

    test

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,5 +1,3 @@
 var currentYear = new Date().getFullYear();
-var copyright01 = document.getElementById("copyright01");
-var copyright02 = document.getElementById("copyright02");
-copyright01.innerHTML = currentYear;
-copyright02.innerHTML = currentYear;
+var copyright = document.getElementById("copyright");
+copyright.innerHTML = currentYear;
