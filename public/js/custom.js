f914f358051591e3c04aa80b7e771e5ec6eafe51
Philip Bautista <philipbautista928@gmail.com>
2023-04-27 14:45:38 +0800

    [update] copyrights final peste

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,3 +1,5 @@
 var currentYear = new Date().getFullYear();
-var copyright = document.getElementsByClassName("copyright_year");
-copyright.innerHTML = currentYear;
+var copyright01 = document.getElementsById("copyright01");
+var copyright02 = document.getElementsById("copyright02");
+copyright01.innerHTML = currentYear;
+copyright02.innerHTML = currentYear;
