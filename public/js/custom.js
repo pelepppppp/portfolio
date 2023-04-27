07fcc988a54a044af7cd80af2c1e90ff943a2017
Philip Bautista <philipbautista928@gmail.com>
2023-04-27 14:40:17 +0800

    [update] copyright updates

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,3 +1,3 @@
 var currentYear = new Date().getFullYear();
 var copyright = document.getElementById("copyright");
-copyright.innerHTML = "&copy; Example Company " + currentYear;
+copyright.innerHTML = currentYear;
