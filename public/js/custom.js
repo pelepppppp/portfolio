f43ff737e3ef6c2de481af81dee1df304c1a964a
Philip Bautista <philipbautista928@gmail.com>
2023-04-27 14:47:05 +0800

    [update] copyright yawa

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,5 +1,5 @@
 var currentYear = new Date().getFullYear();
-var copyright01 = document.getElementsById("copyright01");
-var copyright02 = document.getElementsById("copyright02");
+var copyright01 = document.getElementById("copyright01");
+var copyright02 = document.getElementById("copyright02");
 copyright01.innerHTML = currentYear;
 copyright02.innerHTML = currentYear;
