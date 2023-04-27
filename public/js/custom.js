e8d3e6a7e0c7546820b9b9a717155bc720168b95
Philip Bautista <philipbautista928@gmail.com>
2023-04-27 14:51:27 +0800

    [update] pinakafinal sa copyright

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,3 +1,5 @@
 var currentYear = new Date().getFullYear();
 var copyright = document.getElementById("copyright");
-copyright.innerHTML = "" + currentYear;
+var copyright02 = document.getElementById("copyright_02");
+copyright.innerHTML = currentYear;
+copyright02.innerHTML = currentYear;
