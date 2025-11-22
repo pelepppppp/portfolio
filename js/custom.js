59fe70862d3d291fa69ed16e1d9c678e1afe3187
Philip Bautista <philipbautista927@gmail.com>
2025-11-22 16:17:13 +0800

    latest initial updates nov-22-2025

---
+++ js/custom.js
@@ -0,0 +1,5 @@
+var currentYear = new Date().getFullYear();
+var copyright = document.getElementById("copyright");
+var copyright02 = document.getElementById("copyright_02");
+copyright.innerHTML = currentYear;
+copyright02.innerHTML = currentYear;
